7467ab2fef9b939494aa1b88d9c5548ede9167de
Ian Melendez <ian.melendez.95@utexas.edu>
2021-11-30 23:26:44 -0600

    day 1 part 2

--- src/Day1/Soln.hs
+++ src/Day1/Soln.hs
@@ -4,33 +4,30 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import qualified Data.Text.Read as TR
 
+
 inputFile :: FilePath
 inputFile = "src/Day1/full-input.txt"
+
 
 soln :: IO ()
 soln = do
   content <- TIO.readFile inputFile
-  let increases = countIncreases (parseInput content)
+  let increases = countWindowIncreases (parseDepths content)
   print increases
 
 
-countIncreases :: [Int] -> Int
-countIncreases [] = 0
-countIncreases [_] = 0
-countIncreases (x:y:xs) = (if x < y then 1 else 0) + countIncreases (y:xs)
+countWindowIncreases :: [Int] -> Int
+countWindowIncreases (x:rest@(_:_:y:_)) = (if x < y then 1 else 0) + countWindowIncreases rest
+countWindowIncreases _ = 0
 
 
-parseInput :: T.Text -> [Int]
-parseInput = map parseLine . T.lines
+parseDepths :: T.Text -> [Int]
+parseDepths = map parseLine . T.lines
 
 parseLine :: T.Text -> Int
 parseLine line = 
   case TR.decimal line of 
     Left e -> error e
     Right (val, _) -> val
-
-
-readInput :: IO T.Text
-readInput = TIO.readFile "src/Day1/short-input.txt"
 
 
