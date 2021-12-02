bbd6b600d4480390dbab056439f964c3b01463b2
Ian Melendez <ian.melendez.95@utexas.edu>
2021-12-02 00:45:00 -0600

    d2 p2 - added concise versions of the solution

--- src/Day2/Soln.hs
+++ src/Day2/Soln.hs
@@ -8,6 +8,9 @@ import qualified Data.Text.IO as TIO
 import qualified Data.Text.Read as TR
 
 
+import Data.List (foldl')
+
+
 inputFile :: FilePath
 inputFile = "src/Day2/full-input.txt"
 
@@ -35,6 +38,50 @@ soln =
      putStrLn $ "Horizontal: " ++ show horiz
      putStrLn $ "Depth:      " ++ show depth
      putStrLn $ "Product:    " ++ show (horiz * depth)
+     putStrLn ""
+     putStrLn $ "Fold State: " ++ show (solnWithFold content)
+     putStrLn $ "Arr Ops:    " ++ show (solnWithArrayOps content)
+
+
+solnWithArrayOps :: T.Text -> Int
+solnWithArrayOps input = 
+  let input_lines = T.lines input
+      
+      fwds = map fwdVal input_lines
+
+      cumulative_aims :: [Int]
+      cumulative_aims = scanl1 (+) (map aimVal input_lines)
+
+  in sum fwds * sum (zipWith (*) fwds cumulative_aims)
+  where 
+    fwdVal :: T.Text -> Int
+    fwdVal line = 
+      case T.words line of 
+        ["forward", dist] -> read (T.unpack dist)
+        _ -> 0
+    
+    aimVal :: T.Text -> Int
+    aimVal line = 
+      case T.words line of 
+        ["down", dist] -> read (T.unpack dist)
+        ["up", dist]   -> -(read (T.unpack dist))
+        _ -> 0
+
+
+solnWithFold :: T.Text -> Int
+solnWithFold input = 
+  let (horiz, depth, _) = foldl' fold (0,0,0) (T.lines input)
+  in horiz * depth
+  where 
+    fold :: (Int, Int, Int) -> T.Text -> (Int, Int, Int)
+    fold (horiz, depth, aim) line = 
+      let [dir, dist_str] = T.words line
+          dist = read (T.unpack dist_str) :: Int
+      in case dir of 
+            "forward" -> (horiz + dist, depth + (dist * aim), aim)
+            "up"      -> (horiz, depth, aim - dist)
+            "down"    -> (horiz, depth, aim + dist)
+            _ -> error $ "Unrecognized dir: " ++ T.unpack dir
 
 
 cmdsToTotDisp :: [Cmd] -> Disp
