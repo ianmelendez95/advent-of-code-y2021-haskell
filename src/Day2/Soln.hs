2791811d26d05944a01d9bb5dcb339c32e561a38
Ian Melendez <ian.melendez.95@utexas.edu>
2021-12-01 23:54:11 -0600

    d2 p2 - done

--- src/Day2/Soln.hs
+++ src/Day2/Soln.hs
@@ -14,29 +14,40 @@ inputFile = "src/Day2/full-input.txt"
 -- displacement = (horizontal, depth)
 type Disp = (Int, Int)
 
-type Cmd = (Dir, Int)
+data Cmd = Fwd Int
+         | Aim Int
 
-data Dir = Fwd 
-         | Down
-         | Up
+cmdAimVal :: Cmd -> Int
+cmdAimVal (Fwd _) = 0
+cmdAimVal (Aim v) = v
+
+cmdFwdVal :: Cmd -> Int
+cmdFwdVal (Fwd v) = v
+cmdFwdVal (Aim _) = 0
     
 
 soln :: IO ()
 soln = 
   do content <- TIO.readFile inputFile
 
-     let displacements = map cmdToDisp (parseCmds content)
-         (horiz, depth) = foldr (\(x,y) (xs,ys) -> (x + xs, y + ys)) (0,0) displacements
+     let (horiz, depth) = cmdsToTotDisp (parseCmds content)
 
      putStrLn $ "Horizontal: " ++ show horiz
      putStrLn $ "Depth:      " ++ show depth
      putStrLn $ "Product:    " ++ show (horiz * depth)
 
 
-cmdToDisp :: Cmd -> Disp
-cmdToDisp (Fwd, dist)  = (dist, 0)
-cmdToDisp (Down, dist) = (0, dist)
-cmdToDisp (Up, dist)   = (0, -dist)
+cmdsToTotDisp :: [Cmd] -> Disp
+cmdsToTotDisp cmds = 
+  let cumulative_aim = scanl1 (+) (map cmdAimVal cmds)
+      fwds = map cmdFwdVal cmds
+
+      disps :: (Int, Int)
+      disps = 
+        foldr (\(f, a) (h', d') -> (h' + f, d' + (f * a))) (0,0)
+          . filter ((/= 0) . fst) 
+          $ zip fwds cumulative_aim
+   in disps
 
 
 parseCmds :: T.Text -> [Cmd]
@@ -45,13 +56,12 @@ parseCmds = map parseCmd . T.lines
     parseCmd :: T.Text -> Cmd
     parseCmd input = 
       let [dir, dist] = T.words input
-       in (parseDir dir, parseDecimal dist)
-
-    parseDir :: T.Text -> Dir
-    parseDir "forward" = Fwd
-    parseDir "down" = Down
-    parseDir "up" = Up
-    parseDir input = error (T.unpack input)
+          dist_val = parseDecimal dist
+       in case dir of 
+            "forward" -> Fwd dist_val
+            "down" -> Aim dist_val
+            "up"   -> Aim (-dist_val)
+            _ -> error (T.unpack dir)
 
 parseDecimal :: T.Text -> Int
 parseDecimal line = 
