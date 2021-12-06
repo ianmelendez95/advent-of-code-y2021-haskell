74d62b6b16b4156756cd00b05fe5a2fa0d387d19
Ian Melendez <ian.melendez.95@utexas.edu>
2021-12-06 01:05:31 -0600

    6.2 done

--- src/Day6/Soln.hs
+++ src/Day6/Soln.hs
@@ -12,6 +12,18 @@ import Data.Maybe (listToMaybe, catMaybes)
 import Control.Arrow (Arrow(first))
 
 import qualified Data.Map.Strict as Map
+import qualified Data.IntMap as IntMap
+
+import Control.Monad.State.Lazy
+
+
+type Memo a = State (IntMap.IntMap Int) a
+
+evalMemo :: Memo a -> a
+evalMemo memo = evalState memo IntMap.empty
+
+memoLookup :: Int -> Memo (Maybe Int)
+memoLookup dt = gets (IntMap.lookup dt)
 
 
 inputFile :: FilePath
@@ -22,17 +34,56 @@ soln :: IO ()
 soln = 
   do content <- TIO.readFile inputFile
      let init_fish = parseFish content
-         fish_gens = zip [0..] (iterate iterFish init_fish)
 
-     mapM_ printGen (take 19 fish_gens)
-     putStrLn "..."
-     putStrLn $ "Day 80: " ++ show (length (snd (fish_gens !! 80)))
+     putStrLn $ "Day 80: " ++ show (fishGen 80 init_fish)
+     putStrLn $ "Day 256: " ++ show (fishGen 256 init_fish)
   where 
     printGen :: (Int, [Int]) -> IO ()
     printGen (d, f) = 
       putStrLn $ "Day " ++ show d ++ ": " ++ show (length f) ++ " " ++ show f
 
 
+fishGen :: Int -> [Int] -> Int
+fishGen dt = sum . map (\i -> 1 + evalMemo (nAncestorsMemo (dt - i)))
+
+nAncestorsMemo :: Int -> Memo Int
+nAncestorsMemo dt 
+  | dt <= 0 = pure 0
+  | otherwise = 
+    do existing <- gets (IntMap.lookup dt)
+       case existing of 
+         Just x -> pure x
+         Nothing -> 
+           do n7 <- nAncestorsMemo (dt - 7)
+              n9 <- nAncestorsMemo (dt - 9)
+              let value = 1 + n7 + n9
+              modify (IntMap.insert dt value) 
+              pure value
+
+nAncestors :: Int -> Int
+nAncestors dt
+  | dt > 0 = 1 + nAncestors (dt - 7) + nAncestors (dt - 9)
+  | otherwise = 0
+  
+
+-- nAncestorsWithInc :: Int -> Int -> Int
+-- nAncestorsWithInc incubation dt = 
+--   1 + nAncestors (max 0 (dt - (incubation + 1)))
+
+-- nAncestors :: Int -> Int
+-- nAncestors dt = nChildren dt + sum (map nAncestors (dtRange (dt - 7)))
+--   where 
+--     nChildren :: Int -> Int
+--     nChildren dt = dt `div` 7
+
+--     dtRange :: Int -> [Int]
+--     dtRange dt
+--       | dt <= 0 = []
+--       | otherwise = dt : dtRange (dt - 7)
+
+-- fresh adult = a fish with 6 days left
+-- freshAdultChildren :: Int -> Int
+-- freshAdultChildren days = days `div` 7
 
 iterFish :: [Int] -> [Int]
 iterFish = go 0
