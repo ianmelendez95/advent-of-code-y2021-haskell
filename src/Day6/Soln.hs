3fbcc93ea7ef35b6e6e4d2b427afcf2b54ba8c19
Ian Melendez <ian.melendez.95@utexas.edu>
2021-12-05 23:29:35 -0600

    6.1 done

--- src/Day6/Soln.hs
+++ src/Day6/Soln.hs
@@ -15,14 +15,33 @@ import qualified Data.Map.Strict as Map
 
 
 inputFile :: FilePath
-inputFile = "src/Day6/short-input.txt"
+inputFile = "src/Day6/full-input.txt"
 
 
 soln :: IO ()
 soln = 
   do content <- TIO.readFile inputFile
      let init_fish = parseFish content
-     putStrLn $ "Fish: " ++ show init_fish
+         fish_gens = zip [0..] (iterate iterFish init_fish)
+
+     mapM_ printGen (take 19 fish_gens)
+     putStrLn "..."
+     putStrLn $ "Day 80: " ++ show (length (snd (fish_gens !! 80)))
+  where 
+    printGen :: (Int, [Int]) -> IO ()
+    printGen (d, f) = 
+      putStrLn $ "Day " ++ show d ++ ": " ++ show (length f) ++ " " ++ show f
+
+
+
+iterFish :: [Int] -> [Int]
+iterFish = go 0
+  where 
+    go :: Int -> [Int] -> [Int]
+    go n_new [] = replicate n_new 8
+    go n_new (f:fs)
+      | f <= 0 = 6 : go (n_new + 1) fs
+      | otherwise = f - 1 : go n_new fs
 
 
 parseFish :: T.Text -> [Int]
