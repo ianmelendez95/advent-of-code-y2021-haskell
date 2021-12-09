a458c0aa604bf004aa67cc4c79689ec23d2f88ec
Ian Melendez <ian.melendez.95@utexas.edu>
2021-12-09 00:06:27 -0600

    9.2 done

--- src/Day9/Soln.hs
+++ src/Day9/Soln.hs
@@ -17,6 +17,7 @@ import qualified Data.Map.Strict as Map
 -- import Data.IntMap (IntMap)
 -- import qualified Data.IntMap as IntMap
 
+import Data.Set (Set)
 import qualified Data.Set as Set
 
 import Data.IntSet (IntSet)
@@ -36,10 +37,37 @@ inputFile = "src/Day9/full-input.txt"
 soln :: IO ()
 soln = 
   do points_to_height <- parseInput <$> TIO.readFile inputFile
-     let low_heights = mapMaybe (`heightIfLowPointEntry` points_to_height) (Map.toList points_to_height)
-    --  putStrLn $ "Low Point Heights: " ++ show low_heights
-     putStrLn $ "Risk Sum: " ++ show (sum (map (+1) low_heights))
+     let low_points = mapMaybe (`pointIfLowPointEntry` points_to_height) (Map.toList points_to_height)
+         basins = map (`pointToBasin` points_to_height) low_points
+         largest_basins = take 3 . reverse . sort $ map length basins
      
+    --  mapM_ (\basin -> putStrLn $ show (length basin) ++ ": " ++ show basin) basins
+     print (product largest_basins)
+
+     
+pointToBasin :: Point -> Map.Map Point Int -> [Point]
+pointToBasin point height_map = (Set.toList . Set.fromList) $ pointToBasin' point height_map
+
+pointToBasin' :: Point -> Map.Map Point Int -> [Point]
+pointToBasin' point height_map =
+  let adjacent_basin_points = filter isBasinPoint (adjacentPoints point)
+   in point : concatMap (`pointToBasin` Map.insert point 10 height_map) adjacent_basin_points
+  where 
+    isBasinPoint :: Point -> Bool
+    isBasinPoint adj_point = 
+      let adj_point_height = lookupHeight adj_point height_map
+       in adj_point_height < 9 && adj_point_height >= point_height
+
+    point_height = lookupHeight point height_map
+
+
+
+pointIfLowPointEntry :: (Point, Int) -> Map.Map Point Int -> Maybe Point
+pointIfLowPointEntry (point, point_height) height_map = 
+  let adjacent_heights = map (`lookupHeight` height_map) (adjacentPoints point)
+   in if all (> point_height) adjacent_heights
+        then Just point
+        else Nothing
 
 heightIfLowPointEntry :: (Point, Int) -> Map.Map Point Int -> Maybe Int
 heightIfLowPointEntry (point, point_height) height_map = 
@@ -50,14 +78,10 @@ heightIfLowPointEntry (point, point_height) height_map =
 
 adjacentPoints :: Point -> [Point]
 adjacentPoints (r,c) = 
-  -- [ (r - 1, c - 1)
   [ (r - 1, c)
-  -- , (r - 1, c + 1)
   , (r,     c - 1)
   , (r,     c + 1)
-  -- , (r + 1, c - 1)
   , (r + 1, c)
-  -- , (r + 1, c + 1)
   ]
 
 lookupHeight :: Point -> Map.Map Point Int -> Int
