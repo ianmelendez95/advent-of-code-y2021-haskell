56dd5bd3fd5ca296ce1edfc9eff6c4ecf7dac4a4
Ian Melendez <ian.melendez.95@utexas.edu>
2021-12-16 09:00:36 -0600

    16.1 parse to binary strin

---
+++ src/Day16/Soln.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
+
+module Day16.Soln where 
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import qualified Data.Text.Read as TR
+
+import Data.Ord
+import Data.List
+import Data.Maybe
+import Data.Char
+
+import Data.Map.Lazy (Map)
+import qualified Data.Map.Lazy as Map
+
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+import Data.IntSet (IntSet)
+import qualified Data.IntSet as IntSet
+
+import Control.Monad.State.Lazy
+
+import Debug.Trace
+
+
+inputFile = "src/Day16/short-input-0.txt"
+
+
+soln :: IO ()
+soln = 
+  do bin_str <- parseInput <$> TIO.readFile inputFile
+     putStrLn bin_str
+
+
+parseInput :: T.Text -> String
+parseInput = concatMap hexToBin . T.unpack
+
+hexToBin :: Char -> String
+hexToBin '0' = "0000"
+hexToBin '1' = "0001"
+hexToBin '2' = "0010"
+hexToBin '3' = "0011"
+hexToBin '4' = "0100"
+hexToBin '5' = "0101"
+hexToBin '6' = "0110"
+hexToBin '7' = "0111"
+hexToBin '8' = "1000"
+hexToBin '9' = "1001"
+hexToBin 'A' = "1010"
+hexToBin 'B' = "1011"
+hexToBin 'C' = "1100"
+hexToBin 'D' = "1101"
+hexToBin 'E' = "1110"
+hexToBin 'F' = "1111"
+hexToBin c = error $ "Invalid hex: " ++ [c]
