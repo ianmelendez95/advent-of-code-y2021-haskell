b14962807f5e2b6dcb73b3c53c61175df277a8a2
Ian Melendez <ian.melendez.95@utexas.edu>
2021-12-18 12:01:17 -0600

    18.1 WIP single tree reduction

--- src/Day18/Soln.hs
+++ src/Day18/Soln.hs
@@ -42,6 +42,8 @@ type Parser = Parsec Void T.Text
 data Tree = TNode Tree Tree
           | TInt  Int
 
+type Expl = (Int,Int)
+
 instance Show Tree where 
   show (TInt v) = show v
   show (TNode v1 v2) = "[" ++ show v1 ++ "," ++ show v2 ++ "]"
@@ -80,6 +82,55 @@ treeDepth :: Tree -> Int
 treeDepth (TInt t) = 0
 treeDepth (TNode t1 t2) = 1 + max (treeDepth t1) (treeDepth t2)
 
+-- | Preemptively reduce tree, pending concatenation.
+-- | In other words, reduce as if its depth was 1 more than it 
+-- | is, since that is what it will have right after its concatenated
+-- | with another tree.
+preReduceTree :: Tree -> (Tree, Expl)
+preReduceTree = preReduceTree' 0 (0,0)
+
+preReduceTree' :: Int -> Expl -> Tree -> (Tree, Expl) 
+
+preReduceTree' cur_depth (el, er) t@(TInt v) =
+  let v' = v + el + er
+    in if v' >= 10
+        then preReduceTree' cur_depth (0,0) (splitInt v')
+        else (TInt v', (0,0))
+
+-- TODO: consider propagating explosions with special traversal
+preReduceTree' cur_depth (el, er) t@(TNode tl tr)
+  | cur_depth > 3 = error $ "Depth > 3 shouldn't be possible: " ++ show t
+  | cur_depth == 3 = (TInt 0, intPair t)
+  | otherwise = 
+    let (tl', (trel, trer)) = 
+          preReduceTree' (cur_depth + 1) (0, el) tl
+        (tr', (tlel, tler)) = 
+          preReduceTree' (cur_depth + 1) (er, 0) tr
+        
+        t' = TNode tl' tr'
+      in if tler == 0 && trel == 0
+           then (t', (tlel, trer))
+           else addExpl (tlel, trer) 
+                  <$> preReduceTree' cur_depth (trel, tler) t'
+
+explHasValue :: Expl -> Bool
+explHasValue (e1, e2) = e1 > 0 || e2 > 0
+
+addExpl :: Expl -> Expl -> Expl
+addExpl (e1, e2) (e1', e2') = (e1 + e1', e2 + e2')
+    
+intPair :: Tree -> (Int,Int)
+intPair (TNode (TInt i1) (TInt i2)) = (i1, i2)
+intPair tree = error $ "Expecting number pair but got: " ++ show tree
+
+splitInt :: Int -> Tree
+splitInt v = TNode (TInt (floorDiv v 2)) (TInt (ceilDiv v 2))
+
+floorDiv :: Int -> Int -> Int
+floorDiv = div
+
+ceilDiv :: Int -> Int -> Int
+ceilDiv x y = (x + y - 1) `div` y
 
 
 --------------------------------------------------------------------------------
