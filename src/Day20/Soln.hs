1effb3bcec981ea8d60f4ae5f392d9a5a312980b
Ian Melendez <ian.melendez.95@utexas.edu>
2021-12-20 17:29:54 -0600

    20.1 done

--- src/Day20/Soln.hs
+++ src/Day20/Soln.hs
@@ -52,7 +52,7 @@ type Algo = Vector Bool
 
 
 inputFile :: FilePath
-inputFile = "src/Day20/short-input.txt"
+inputFile = "src/Day20/full-input.txt"
 
 
 soln :: IO ()
@@ -61,20 +61,25 @@ soln =
 
      let image' = enhancedImage algo image
          image'' = enhancedImage algo image'
+         image_2e = twiceEnhancedImage algo image
 
      putStrLn $ showAlgo algo
 
-     putStrLn "[Show me the evidence]"
-     putStrLn $ "Lit: " ++ show (Set.size image)
-     putStrLn $ showImage image
+    --  putStrLn "[Show me the evidence]"
+    --  putStrLn $ "Lit: " ++ show (Set.size image)
+    --  putStrLn $ showImage image
 
-     putStrLn "\n[ENHANCE!]"
-     putStrLn $ "Lit: " ++ show (Set.size image')
-     putStrLn $ showImage image'
+    --  putStrLn "\n[ENHANCE!]"
+    --  putStrLn $ "Lit: " ++ show (Set.size image')
+    --  putStrLn $ showImage image'
 
-     putStrLn "\n[ENHANCE AGAIN!]"
-     putStrLn $ "Lit: " ++ show (Set.size image'')
-     putStrLn $ showImage image''
+    --  putStrLn "\n[ENHANCE AGAIN!]"
+    --  putStrLn $ "Lit: " ++ show (Set.size image'')
+    --  putStrLn $ showImage image''
+
+     putStrLn "\n[OPTIMAL ENHANCE]"
+     putStrLn $ "Lit: " ++ show (Set.size image_2e)
+    --  putStrLn $ showImage image_2e
   where 
     showAlgo :: Algo -> String
     showAlgo = map (\on -> if on then '#' else '.') . V.toList
@@ -99,9 +104,23 @@ enhancedImage :: Algo -> Image -> Image
 enhancedImage algo image = 
   Set.fromList $ filter (enhancedPoint algo image) (imageRegion image)
 
+twiceEnhancedImage :: Algo -> Image -> Image
+twiceEnhancedImage algo image =
+  Set.fromList $ filter (twiceEnhancedPoint algo image) (twiceImageRegion image)
+
 imageRegion :: Image -> [Point]
 imageRegion = concatMap cluster . Set.toList
 
+twiceImageRegion :: Image -> [Point]
+twiceImageRegion = concatMap cluster . concatMap cluster . Set.toList
+
+
+twiceEnhancedPoint :: Algo -> Image -> Point -> Bool
+twiceEnhancedPoint algo image point = 
+  let point_cluster = cluster point
+      enhanced_cluster = map (enhancedPoint algo image) point_cluster
+      enhanced_cluster_value = binStrToDecimal enhanced_cluster
+   in algo V.! enhanced_cluster_value
         
 enhancedPoint :: Algo -> Image -> Point -> Bool
 enhancedPoint algo image point = algo V.! pixelValue image point
@@ -109,9 +128,11 @@ enhancedPoint algo image point = algo V.! pixelValue image point
 -- | TODO - this assumes that background pixels are always 0
 pixelValue :: Image -> Point -> Int
 pixelValue image point = 
-  let cs = cluster point
-      bin_str = map (\cp -> if cp `Set.member` image then 1 else 0) (cluster point)
-   in foldl' (\total bit -> 2 * total + bit) 0 bin_str
+  let bin_str = map (`Set.member` image) (cluster point)
+   in binStrToDecimal bin_str
+
+binStrToDecimal :: [Bool] -> Int
+binStrToDecimal = foldl' (\total bit -> 2 * total + bit) 0 . map (\b -> if b then 1 else 0)
 
 cluster :: Point -> [Point]
 cluster (cm, cn) = 
