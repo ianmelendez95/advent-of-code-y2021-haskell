cc5cbb6e00f94a303d62479a49867f3617657410
Ian Melendez <ian.melendez.95@utexas.edu>
2021-12-20 09:46:36 -0600

    20.1 parsing and printing input

--- src/Day20/Soln.hs
+++ src/Day20/Soln.hs
@@ -33,7 +33,9 @@ import Data.Void
 
 import Debug.Trace
 import Control.Monad.Combinators.Expr 
-import System.Posix.Internals (o_RDWR)
+
+import Data.Vector.Unboxed (Vector)
+import qualified Data.Vector.Unboxed as V
 
 traceMsgId :: (Show a) => String -> a -> a
 traceMsgId msg x = traceMsgShow msg x x 
@@ -44,26 +46,64 @@ traceMsgShow msg x = trace (msg ++ ": " ++ show x)
 type Parser = Parsec Void T.Text
 
 
+type Point = (Int, Int)
+type Image = Set Point
+type Algo = Vector Bool
+
+
+inputFile :: FilePath
 inputFile = "src/Day20/short-input.txt"
 
 
 soln :: IO ()
 soln = 
-  do (algo, image) <- readInput <$> TIO.readFile inputFile
+  do (algo, image) <- parseInput <$> TIO.readFile inputFile
 
-     TIO.putStrLn algo
+     putStrLn $ showAlgo algo
+
      putStrLn ""
-     mapM_ TIO.putStrLn image
+
+     putStrLn $ showImage image
+  where 
+    showAlgo :: Algo -> String
+    showAlgo = map (\on -> if on then '#' else '.') . V.toList
+
+    showImage :: Image -> String
+    showImage img = 
+      let (low_m, low_n) = Set.findMin img
+          (high_m, high_n) = Set.findMax img
+
+          rows :: [String]
+          rows = map (\r -> map (\c -> if (r,c) `Set.member` img then '#' else '.') [low_n..high_n]) [low_m..high_m]
+       in unlines rows
+
+        
 
 
-triples :: T.Text -> [T.Text]
-triples txt = 
-  let triple = T.take 3 txt
-   in if T.length triple == 3 
-        then triple : triples (T.tail txt)
-        else []
+cluster :: Point -> [Point]
+cluster (cm, cn) = 
+  [ (cm - 1, cn - 1) 
+  , (cm - 1, cn)
+  , (cm - 1, cn + 1)
+  , (cm, cn - 1) 
+  , (cm, cn)
+  , (cm, cn + 1)
+  , (cm + 1, cn - 1) 
+  , (cm + 1, cn)
+  , (cm + 1, cn + 1)
+  ]
 
-readInput :: T.Text -> (T.Text, [T.Text])
-readInput input = 
-  let (algo:_:image) = T.lines input 
-   in (algo, image)
+parseInput :: T.Text -> (Algo, Image)
+parseInput input = 
+  let (algo_line:_:image_lines) = T.lines input 
+      algo = V.fromList (map (== '#') (T.unpack algo_line))
+   in (algo, parseImageLines image_lines)
+
+parseImageLines :: [T.Text] -> Set Point
+parseImageLines lines = 
+  let rows = zip [0..] lines
+   in Set.fromList $ concatMap (uncurry parseLine) rows
+  where 
+    parseLine :: Int -> T.Text -> [Point]
+    parseLine row line = map ((row,) . fst) . filter ((== '#') . snd) $ zip [0..] (T.unpack line)
+
