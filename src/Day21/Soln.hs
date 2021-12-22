defbb594b8fe3dcebd435600bde7c00f12bb8ffe
Ian Melendez <ian.melendez.95@utexas.edu>
2021-12-21 21:49:11 -0600

    21.1 close, don't have loser score

--- src/Day21/Soln.hs
+++ src/Day21/Soln.hs
@@ -48,12 +48,61 @@ type Parser = Parsec Void T.Text
 
 
 inputFile :: FilePath
-inputFile = "src/Day20/full-input.txt"
+inputFile = "src/Day21/short-input.txt"
 
 
 soln :: IO ()
 soln = 
-  do print (take 5 (roundPositions (4, 8)))
+  do init_pos@(p1,p2) <- parseInput <$> TIO.readFile inputFile
+
+     putStrLn "\n[Initial Positions]"
+     print (p1,p2)
+
+     let positions = roundPositions init_pos
+         scores = cumSumPairs (drop 1 positions)
+         idx_scores = zip [0..] scores
+        
+         (Just win_round) = find (isWinRound . snd) idx_scores
+
+     putStrLn "\n[Round Positions]"
+     mapM_ print (take 5 positions)
+    
+     putStrLn "\n[Round Scores]"
+     mapM_ print (take 5 idx_scores)
+
+     putStrLn "\n[Winning Round]"
+     print win_round
+     putStrLn $ "Rolls: " ++ show (winRoundRolls win_round)
+
+    --  putStrLn "\n[Result]"
+    --  print (winRoundRolls win_round * loserScore (snd win_round))
+  where 
+    isWinRound :: (Int,Int) -> Bool
+    isWinRound (s1,s2) = s1 >= 1000 || s2 >= 1000
+
+    winRoundRolls :: (Int,(Int,Int)) -> Int
+    winRoundRolls r@(round,(s1, s2))
+      | s1 >= 1000 = ((round - 1) * 6) + 3
+      | s2 >= 1000 = ((round - 1) * 6) + 6
+      | otherwise = error $ "No winner: " ++ show r
+
+    loserScore :: (Int,Int) -> Int
+    loserScore (s1, s2)
+      | s1 <= s2 = s1
+      | otherwise = s2
+     
+
+roundScores :: (Int,Int) -> [(Int,Int)]
+roundScores = cumSumPairs . roundPositions
+
+cumSumPairs :: [(Int,Int)] -> [(Int,Int)]
+cumSumPairs = scanl' doSum (0,0)
+  where 
+    doSum :: (Int,Int) -> (Int,Int) -> (Int,Int)
+    doSum (s1, s2) (x1, x2) = 
+      let s1' = s1 + x1
+          s2' = s2 + x2
+       in s1' `seq` s2' `seq` (s1', s2')
 
 
 roundPositions :: (Int,Int) -> [(Int,Int)]
@@ -76,3 +125,11 @@ rollSums = go [1..]
           (p2_rolls, rolls'') = splitAt 3 rolls'
        in (sum p1_rolls, sum p2_rolls) : go rolls''
 
+
+parseInput :: T.Text -> (Int,Int)
+parseInput input = 
+  let [p1,p2] = map parseLine (T.lines input)
+   in (p1,p2)
+  where 
+    parseLine :: T.Text -> Int
+    parseLine = read . T.unpack . last . T.words
